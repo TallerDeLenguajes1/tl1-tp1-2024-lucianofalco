64d6d7b71041d05875896780952f7e4e0b9de4a9
“LucianoFalco” <lucianosebastianfalco@gmail.com>
2024-03-26 15:44:23 -0300

    punteros

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,7 +1,23 @@
 #include<stdio.h>
+/*e) Declare un puntero en el procedimiento principal que apunte a una
+variable de algún tipo y muestre por pantalla lo siguiente:
+1) El contenido del puntero
+2) La dirección de memoria almacenada por el puntero.
+3) la dirección de memoria de la variable.
+4) la dirección de memoria del puntero.
+5) el tamaño de memoria utilizado por esa variable usando la
+función sizeof()
+*/
 int main(int argc, char const *argv[])
 {
     printf("Hola mundo");
-    printf("Hola mundo");
+    int valor = 10;
+    int *p_valor = &valor;
+
+    printf ("\nEl valor es : %d\n" , valor);
+    printf ("\nY su direccion de memoria es: %p\n" , p_valor);
+
+
+
     return 0;
 }
